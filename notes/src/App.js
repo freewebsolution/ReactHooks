f4d650878d085bc86c51f301f80b9c7a685ddce4
lucio Ticali <info@freewebsolution.it>
2021-03-19 11:34:06 +0100

    Aggiungi una nuova nota

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
+import React, { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ note }) => {
+const App = (props) => {
+  const [note, setNote] = useState(props.note)
+  const [newNote, setNewNote] = useState('')
+  const addNote = (e) => {
+    e.preventDefault()
+    const noteObj = {
+      tema: newNote,
+      data: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: note.lenght + 1
+    }
+    setNote(note.concat(noteObj))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
   return (
     <div className="App">
       <h1>Note</h1>
       <ul>
         {note.map(nota =>
-          <Note key={nota.id} nota={nota}/>
+          <Note key={nota.id} nota={nota} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          placeholder="Aggiungi nota..."
+          onChange={handleNoteChange}
+          value={newNote}
+        />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Note = ({nota}) => {
+const Note = ({ nota }) => {
     return (
         <div>
             <li>{nota.tema} il giorno {nota.giorno} alle ore {nota.ora}</li>
