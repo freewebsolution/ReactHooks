a0e2d33524de7bfcb16ccc618aa686a38109ec98
lucio Ticali <info@freewebsolution.it>
2021-03-20 11:23:03 +0100

    Installa axios

--- notes/src/index.js
+++ notes/src/index.js
@@ -3,40 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+axios
+  .get('http://localhost:3001/note')
+  .then(res => {
+    const note = res.data
+    ReactDOM.render(
+      <React.StrictMode>
+        <App note={note} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
 
-const note = [
-  {
-    id:1,
-    tema:'Appuntamento dal dentista',
-    data:'2021-03-18T9:30.0981Z',
-    giorno:'21/03/2021',
-    ora:'9:30',
-    important:true
-  },
-  {
-    id: 2,
-    tema: 'Appuntamento dal commercialista',
-    data: '2021-03-18T9:30.0981Z',
-    giorno: '21/03/2021',
-    ora: '14:30',
-    important:false
-  },
-  {
-    id: 3,
-    tema: 'Andare a scuola a prendere Giorgia',
-    data: '2021-03-18T9:30.0981Z',
-    giorno: '21/03/2021',
-    ora: '15:30',
-    important:true
-  },
-  
-]
-ReactDOM.render(
-  <React.StrictMode>
-    <App note= {note}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
